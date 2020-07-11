eeb1da10900f1f77107456ab8bedbdd2f92e993b
Livia Wijayanti <liviawijayanti@gmail.com>
2020-07-12 00:06:15 +1000

    translate file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {Component, useState} from 'react';
 import './App.css';
 import ChatBot from 'react-simple-chatbot';
 import { ThemeProvider } from 'styled-components';
 import BobRoss from './bob-ross.jpg';
 import BotSentinel from './bot-sentinel.jpg';
+import PropTypes from 'prop-types';
+import Dropzone from 'react-dropzone-uploader'
+import filledForm from './filled-form.pdf';
+
 
 const theme = {
   background: '#f5f8fb',
@@ -17,13 +21,49 @@ const theme = {
   userFontColor: '#4a4a4a',
 };
 
+
+///
+
+
+class Filled extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+
+  render() {
+    return (
+        <a href="./filled-form.pdf"> Your filled in Form </a>
+      )
+
+    
+  }
+}
+
+
+
+
+//
+
+
 function App() {
+  const getUploadParams = ({ meta }) => { return { url: 'https://httpbin.org/post' } }
+  
+  // called every time a file's `status` changes
+  const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
+  
+  // receives array of files that are done uploading when submit button is clicked
+  const handleSubmit = (files) => { console.log(files.map(f => f.meta)) }
+
+
+
   return (
     <div className="App">
       <div id='centreDiv'>
         <ThemeProvider theme={theme}>
           <ChatBot
-              headerTitle={'Hoogle Bot'}
+              headerTitle={'KlueBot'}
               steps={[
                 {
                   id: 'welcome',
@@ -45,7 +85,83 @@ function App() {
                   end: true
                 }, {
                   id: 'covid-start',
-                  message: 'NOT IMPLEMENTED',
+                  message: "What form of help do you need?",
+                  trigger: 'covid-options'
+                },
+                {
+                  id: 'covid-options',
+                  options: [
+                    { value: 1, label: 'Fill in Form', trigger: 'covid-form' },
+                    { value: 2, label: 'Translate Form to a different language', trigger: 'translate-covid-form' },
+                    { value: 3, label: 'Information', trigger: 'covid-info' }
+                  ]
+                },
+                { 
+                  id: 'translate-covid-form',
+                  component: <Dropzone
+                    getUploadParams={getUploadParams}
+                    onChangeStatus={handleChangeStatus}
+                    onSubmit={handleSubmit}
+                    accept="image/*,audio/*,video/*"
+                  />,
+                  end: true
+                },
+
+                {
+                  id: 'covid-info',
+                  message: 'Here are the info about COVID-19?',
+                  trigger: 'covid-form-finance'
+                },
+                {
+                  id: 'covid-form',
+                  message: 'What form do you need?',
+                  trigger: 'covid-form-user'
+                },
+                {
+                  id: 'covid-form-user',
+                  user: true,
+                  trigger: 'covid-form-finance'
+                },
+                {
+                  id: 'covid-form-finance',
+                  message: "what is your preferable language?",
+                  trigger: 'user-form-language'
+                }, 
+                {
+                  id: 'user-form-language',
+                  user: true,
+                  trigger: 'covid-form-name'
+                }, 
+                {
+                  id: 'covid-form-name',
+                  message: "cual es tu nombre legal completo",
+                  trigger: 'user-form-legal-name'
+                }, 
+                {
+                  id: 'user-form-legal-name',
+                  user: true,
+                  trigger: 'what-is-your-age'
+                }, 
+                {
+                  id: 'what-is-your-age',
+                  message: "Cuál es tu edad",
+                  trigger: 'user-what-is-your-age'
+                }, 
+                
+
+                {
+                  id: 'user-what-is-your-age',
+                  user: true,
+                  trigger: 'filled-form'
+                }, 
+                {
+                  id: 'filled-form',
+                  message: "aquí está tu forma",
+                  trigger: 'give-filled-form'
+                },
+                {
+                  id:'give-filled-form',
+                  component: <Filled/>,
                   end: true
                 }
               ]}
