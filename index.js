787ee53a1e948e60b615c7e581051e93dc2fac70
Annabel Zhou <ayzhou00@gmail.com>
2020-07-11 13:01:00 +1000

    added tokens and ngrok

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 'use strict';
 const BootBot = require('./lib/BootBot');
 
+const bot = new BootBot({
+    accessToken: 'EAAEHVbsSPc0BAOfnyEOg1t2GjzEaZAmyVK37v8cZCx7WFdNzJZB6ZAgFKoQEZBqa7VIdvhSwZBn1SSQaG6ZBN7V5DbEno1Cr79Gqb01mYZBryKGlAVpC7C1Uut0GiFO1s1isHkehq6aq01Ccv2qLfjZAhqd6kpMMk9qFq1Yx4ndguTZCWmPdZBa7tzPQZBCuL3I9RuAZD',
+    verifyToken: 'hoogle',
+    appSecret: '33782e8a86bfc6d6c0f975cee74202dd'
+  });
+
 // Imports dependencies and set up http server
 const
   express = require('express'),
@@ -63,5 +69,83 @@ app.get('/webhook', (req, res) => {
         }
     }
 });
+
+////////////////////
+
+bot.on('message', (payload, chat) => {
+	const text = payload.message.text;
+	console.log(`The user said: ${text}`);
+});
+
+bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
+	console.log('The user said "hello", "hi", "hey", or "hey there"');
+});
+
+bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
+	// Send a text message followed by another text message that contains a typing indicator
+	chat.say('Hello, human friend!').then(() => {
+		chat.say('How are you today?', { typing: true });
+	});
+});
+
+bot.hear(['food', 'hungry'], (payload, chat) => {
+	// Send a text message with quick replies
+	chat.say({
+		text: 'What do you want to eat today?',
+		quickReplies: ['Mexican', 'Italian', 'American', 'Argentine']
+	});
+});
+
+bot.hear(['help'], (payload, chat) => {
+	// Send a text message with buttons
+	chat.say({
+		text: 'What do you need help with?',
+		buttons: [
+			{ type: 'postback', title: 'Settings', payload: 'HELP_SETTINGS' },
+			{ type: 'postback', title: 'FAQ', payload: 'HELP_FAQ' },
+			{ type: 'postback', title: 'Talk to a human', payload: 'HELP_HUMAN' }
+		]
+	});
+});
+
+bot.hear('image', (payload, chat) => {
+	// Send an attachment
+	chat.say({
+		attachment: 'image',
+		url: 'http://example.com/image.png'
+	});
+});
+
+bot.hear('ask me something', (payload, chat) => {
+
+	const askName = (convo) => {
+		convo.ask(`What's your name?`, (payload, convo) => {
+			const text = payload.message.text;
+			convo.set('name', text);
+			convo.say(`Oh, your name is ${text}`).then(() => askFavoriteFood(convo));
+		});
+	};
+
+	const askFavoriteFood = (convo) => {
+		convo.ask(`What's your favorite food?`, (payload, convo) => {
+			const text = payload.message.text;
+			convo.set('food', text);
+			convo.say(`Got it, your favorite food is ${text}`).then(() => sendSummary(convo));
+		});
+	};
+
+	const sendSummary = (convo) => {
+		convo.say(`Ok, here's what you told me about you:
+	      - Name: ${convo.get('name')}
+	      - Favorite Food: ${convo.get('food')}`);
+      convo.end();
+	};
+
+	chat.conversation((convo) => {
+		askName(convo);
+	});
+});
+
+bot.start();
 
 module.exports = BootBot;
