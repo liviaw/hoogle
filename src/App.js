6129e3ca5648a22dd3e06a5a86e0c8ab2d6884d7
Michael Gribben <michaelgribbeninfo@gmail.com>
2020-07-11 20:06:57 +1000

    fixed bug

--- src/App.js
+++ src/App.js
@@ -11,19 +11,25 @@ function App() {
             {
               id: 'welcome',
               message: 'Welcome to DisasterBot! Do you want bushfire or covid relief?',
-              trigger: 'welcome-user'
-            }, {
-              id: 'welcome-user',
-              user: true,
               trigger: 'disaster-start'
             }, {
               id: 'disaster-start',
               options: [
-                { label: 'bushfire', value: 1, trigger: 'bushfire-start' },
-                { label: 'covid-start', value: 2, trigger: 'covid-start'}
+                { value: 1, label: 'bushfire', trigger: 'bushfire-start' },
+                { value: 2, label: 'covid', trigger: 'covid-start' }
               ]
             }, {
-              
+              id: 'bushfire-start',
+              message: 'I will just ask a few questions. At the end, I will send you everything I know to help you :)',
+              trigger: 'bushfire-business-owner'
+            }, {
+              id: 'bushfire-business-owner',
+              user: true,
+              end: true
+            }, {
+              id: 'covid-start',
+              message: 'NOT IMPLEMENTED',
+              end: true
             }
           ]}
         />
