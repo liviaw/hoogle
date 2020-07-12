afe48dfb755bc6932cabcb8b158eee08f075ac3d
Michael Gribben <michaelgribbeninfo@gmail.com>
2020-07-12 12:24:52 +1000

    added build

---
+++ build/precache-manifest.64f9e8c413bb7d4769efffe5e972f858.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021e831a5df1643e1a7e40e6da553c69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1577442a9d88f10253fa",
+    "url": "/static/css/main.1506822d.chunk.css"
+  },
+  {
+    "revision": "24bacaf6fe7fc4c4da47",
+    "url": "/static/js/2.146396aa.chunk.js"
+  },
+  {
+    "revision": "3d00338f00caa758609ab2bae3ddd8e8",
+    "url": "/static/js/2.146396aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1577442a9d88f10253fa",
+    "url": "/static/js/main.6bdffdef.chunk.js"
+  },
+  {
+    "revision": "0e5dc210f421aea4b19a",
+    "url": "/static/js/runtime-main.7aeb85a8.js"
+  },
+  {
+    "revision": "3baa4bfcd7ce4c5c01276b39032141e3",
+    "url": "/static/media/bob-ross.3baa4bfc.jpg"
+  },
+  {
+    "revision": "7971d029cedee454d6b3df6aceccb593",
+    "url": "/static/media/bot-sentinel.7971d029.jpg"
+  },
+  {
+    "revision": "dd9b28c5b473d7424bea81a149cc1f63",
+    "url": "/static/media/disaster-background.dd9b28c5.jpg"
+  }
+]);
